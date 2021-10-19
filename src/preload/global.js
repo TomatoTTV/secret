101eb11271c887b986a1b6585bce1c411f5377f2
TomatoTTV <60125355+TomatoTTV@users.noreply.github.com>
2021-10-18 22:42:54 -0400

    New Splash screen & Css

--- src/preload/global.js
+++ src/preload/global.js
@@ -43,6 +43,7 @@ function doOnLoad() {
     resetVars();
     let html = `
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://tomatottv.github.io/mainCssV2/styleV2.css">
     <style>
 
     #show-clientNotif{
